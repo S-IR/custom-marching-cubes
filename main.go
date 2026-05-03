0055e82be3e26417edcdedddedab2b3c1354c79f
stan <stitrzv@proton.me>
2026-05-03 11:13:59 +0300

    save

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Point struct {
 	p bool
@@ -8,41 +10,45 @@ type Point struct {
 }
 
 func ccw_add_faces(left, right, topLeft, topRight Point) (res []int) {
+	// if !left.p || !right.p || !topLeft.p || !topRight.p {
+	// 	return
+	// }
 	if left.p && right.p && topRight.p {
 		res = append(res, left.i, right.i, topRight.i)
 	}
 	if left.p && topRight.p && topLeft.p {
 		res = append(res, left.i, topRight.i, topLeft.i)
-
 	}
 
-	if !left.p && right.p && topLeft.p && topRight.p {
-		res = append(res, topLeft.i, right.i, topRight.i)
-	}
+	// if !left.p && right.p && topLeft.p && topRight.p {
+	// 	res = append(res, topLeft.i, right.i, topRight.i)
+	// }
 
-	if !topRight.p && left.p && right.p && topLeft.p {
-		res = append(res, left.i, right.i, topLeft.i)
-	}
+	// if !topRight.p && left.p && right.p && topLeft.p {
+	// 	res = append(res, left.i, right.i, topLeft.i)
+	// }
 
 	return res
 }
-func ccw_checker_pattern(checkerLeft, checkerRight, checkerTopLeft, checkerTopRight Point, badCheckerTopLeft, badCheckerTopRight, badCheckerLeft, badCheckerRight Point) (res []int) {
 
-	checker1True := checkerLeft.p && checkerRight.p && checkerTopLeft.p && checkerTopRight.p
-	checker2True := badCheckerLeft.p && badCheckerRight.p && badCheckerTopLeft.p && badCheckerTopRight.p
+func ccw_checker_pattern(checkerLeft, checkerRight, checkerTopLeft, checkerTopRight Point, badCheckerBottomLeft, badCheckerBottomRight, badCheckerTopLeft, badCheckerTopRight Point) (res []int) {
 
-	checker1AllFalse := checkerLeft.p == false && checkerRight.p == false && checkerTopLeft.p == false && checkerTopRight.p == false
-	checker2AllFalse := badCheckerLeft.p == false && badCheckerRight.p == false && badCheckerTopLeft.p == false && badCheckerTopRight.p == false
+	checkerTrue := checkerLeft.p && checkerRight.p && checkerTopLeft.p && checkerTopRight.p
+	// checker2True := badCheckerLeft.p && badCheckerRight.p && badCheckerTopLeft.p && badCheckerTopRight.p
+	badCheckerFalse := (badCheckerBottomLeft.p == false && badCheckerBottomRight.p == false)
+	badCheckerFalse = badCheckerFalse || (badCheckerTopLeft.p == false && badCheckerTopRight.p == false)
+	// checker1AllFalse := checkerLeft.p == false && checkerRight.p == false && checkerTopLeft.p == false && checkerTopRight.p == false
+	// checker2AllFalse := badCheckerLeft.p == false && badCheckerRight.p == false && badCheckerTopLeft.p == false && badCheckerTopRight.p == false
 
-	if checker1True && checker2AllFalse {
-		res = append(res, checkerLeft.i, checkerRight.i, checkerTopRight.i)
-		res = append(res, checkerLeft.i, checkerTopRight.i, checkerTopLeft.i)
+	if checkerTrue && badCheckerFalse {
+		res = append(res, ccw_add_faces(checkerLeft, checkerRight, checkerTopLeft, checkerTopRight)...)
+		// res = append(res, checkerLeft.i, checkerTopRight.i, checkerTopLeft.i)
 	}
 
-	if checker2True && checker1AllFalse {
-		res = append(res, badCheckerLeft.i, badCheckerRight.i, badCheckerTopRight.i)
-		res = append(res, badCheckerLeft.i, badCheckerTopRight.i, badCheckerTopLeft.i)
-	}
+	// if checker2True && checker1AllFalse {
+	// 	res = append(res, ccw_add_faces(badCheckerLeft, badCheckerRight, badCheckerTopLeft, badCheckerTopRight)...)
+	// 	// res = append(res, badCheckerLeft.i, badCheckerTopRight.i, badCheckerTopLeft.i)
+	// }
 	return res
 }
 
@@ -67,6 +73,42 @@ func ccw_add_edges(sideBottom, sideTop Point, otherDiagonalLeft, otherDiagonalRi
 	return res
 
 }
+
+var edges = [6][2]int{
+
+	{0, 2},
+	{1, 3},
+	{0, 1},
+	{2, 3},
+	{0, 4},
+	{1, 5},
+}
+
+var aboveEdges = [12][2]int{
+	{4, 6},
+	{5, 7},
+	{4, 5},
+	{6, 7},
+	{3, 7},
+	{2, 6},
+
+	{1, 3},
+	{0, 2},
+	{2, 3},
+	{0, 1},
+	{2, 4},
+	{1, 5},
+}
+
+var oppositeEdges = [6][2]int{
+	{5, 7},
+	{4, 6},
+	{6, 7},
+	{4, 5},
+	{3, 7},
+	{2, 6},
+}
+
 func custom_marching_cubes() [256][]int {
 	finalIndices := [256][]int{}
 	was0 := false
@@ -96,33 +138,42 @@ func custom_marching_cubes() [256][]int {
 		p6 := Point{p: has6, i: 6}
 		p7 := Point{p: has7, i: 7}
 
+		// ps := []Point{p0, p1, p2, p3, p4, p5, p6, p7}
 		indices := []int{}
+		if i == 175 {
+			// fmt.Println("front:", ccw_add_faces(p0, p2, p4, p6))
+			// fmt.Println("back:", ccw_add_faces(p3, p1, p7, p5))
+			// fmt.Println("left:", ccw_add_faces(p1, p0, p5, p4))
+			// fmt.Println("right:", ccw_add_faces(p2, p3, p6, p7))
+			// fmt.Println("bottom:", ccw_add_faces(p1, p3, p0, p2))
+			// fmt.Println("top:", ccw_add_faces(p4, p6, p5, p7))
+			// fmt.Println("edge1:", ccw_add_edges(p2, p6, p0, p3, p4, p7, p1, p5))
+			// fmt.Println("edge2:", ccw_add_edges(p0, p4, p1, p2, p5, p6, p3, p7))
+			// fmt.Println("edge3:", ccw_add_edges(p3, p7, p2, p1, p6, p5, p0, p4))
+			// fmt.Println("edge4:", ccw_add_edges(p1, p5, p3, p0, p7, p4, p2, p6))
+			// fmt.Println("checker1:", ccw_checker_pattern(p0, p2, p5, p7, p4, p6, p1, p3))
+			// fmt.Println("checker2:", ccw_checker_pattern(p2, p3, p4, p5, p6, p7, p0, p1))
 
+			// fmt.Println("checker2:", ccw_checker_pattern(p1, p0, p7, p7, p5, p4, p3, p2))
+		}
 		indices = append(indices, ccw_add_faces(p0, p2, p4, p6)...)
-		// indices = append(indices, ccw_add_faces(p3, p1, p7, p5)...)
+		indices = append(indices, ccw_add_faces(p3, p1, p7, p5)...)
 
 		indices = append(indices, ccw_add_faces(p1, p0, p5, p4)...)
-		// indices = append(indices, ccw_add_faces(p2, p3, p6, p7)...)
+		indices = append(indices, ccw_add_faces(p2, p3, p6, p7)...)
 
-		// indices = append(indices, ccw_add_faces(p7, p5, p6, p4)...)
-		indices = append(indices, ccw_add_faces(p1, p3, p0, p2)...)
-
-		indices = append(indices, ccw_add_edges(p2, p6, p0, p3, p4, p7, p1, p5)...)
-		indices = append(indices, ccw_add_edges(p0, p4, p1, p2, p5, p6, p3, p7)...)
-		indices = append(indices, ccw_add_edges(p3, p7, p2, p1, p6, p5, p0, p4)...)
-		indices = append(indices, ccw_add_edges(p1, p5, p3, p0, p7, p4, p2, p6)...)
-
-		indices = append(indices, ccw_checker_pattern(p0, p2, p5, p7, p4, p6, p1, p3)...)
-		indices = append(indices, ccw_checker_pattern(p1, p0, p7, p7, p5, p4, p3, p2)...)
-
-		// indices = append(indices, ccw_checker_pattern(p2, p3, p4, p5, p6, p7, p0, p1)...)
-		// indices = append(indices, ccw_checker_pattern(p3, p1, p6, p4, p7, p5, p2, p0)...)
+		indices = append(indices, ccw_add_faces(p0, p2, p1, p3)...)
+		indices = append(indices, ccw_add_faces(p4, p6, p5, p7)...)
 
 		finalIndices[i] = indices
 
 	}
 	// maxLen := 36
 
+	return finalIndices
+}
+func main() {
+	finalIndices := custom_marching_cubes()
 	fmt.Println("POINTS_TO_TRIANGLES_CONVERTER:=[256][]i32{")
 
 	for _, p := range finalIndices {
@@ -153,8 +204,27 @@ func custom_marching_cubes() [256][]int {
 	}
 	fmt.Println("};")
 
-	return finalIndices
 }
-func main() {
-	custom_marching_cubes()
+func intAbs(v int) int {
+	if v < 0 {
+		v *= -1
+	}
+	return v
+}
+func getP(val int, ps []Point) Point {
+	for _, p := range ps {
+		if p.i == val {
+			return p
+		}
+	}
+	panic(fmt.Sprintf("did not find p for val %v , ps: %v", val, ps))
+}
+func pointsToUint8(points ...int) uint8 {
+	var result uint8
+	for _, p := range points {
+		if p >= 0 && p <= 7 {
+			result |= 1 << p
+		}
+	}
+	return result
 }
--- main_test.go
+++ main_test.go
@@ -8,15 +8,6 @@ import (
 )
 
 // pointsToUint8 takes a slice of ints [0..7] and returns a uint8 with those bits set
-func pointsToUint8(points ...int) uint8 {
-	var result uint8
-	for _, p := range points {
-		if p >= 0 && p <= 7 {
-			result |= 1 << p
-		}
-	}
-	return result
-}
 
 func TestMarchingCubes(t *testing.T) {
 	indices := custom_marching_cubes()
@@ -49,6 +40,9 @@ func TestMarchingCubes(t *testing.T) {
 
 		TestStruct{index: pointsToUint8(0, 2, 4, 6), indicesExpected: FRONT_FACE[:]},
 		TestStruct{index: pointsToUint8(4, 5, 6, 7), indicesExpected: TOP_FACE[:]},
+		TestStruct{index: pointsToUint8(2, 3, 4, 5), indicesExpected: []int{4, 5, 3, 4, 3, 2}},
+
+		TestStruct{index: pointsToUint8(0, 1, 2, 3, 5, 7), indicesExpected: concat(BOTTOM_FACE[:], BACK_FACE[:], []int{1, 0, 5, 2, 3, 7, 5, 7, 2, 5, 2, 0})},
 
 		TestStruct{index: pointsToUint8(7, 6, 5, 4), indicesExpected: TOP_FACE[:]},
 		// TestStruct{index: pointsToUint8(4, 6, 5, 7), indicesExpected: []int{4, 6, 7, 4, 7, 5}},
@@ -80,18 +74,22 @@ func TestMarchingCubes(t *testing.T) {
 		// TestStruct{index: pointsToUint8(0,1,4,5,3,7,2), indicesExpected: []int{}},
 
 	}
+	// fmt.Println("pointsToUint8(0, 1, 2, 3, 5, 7),", pointsToUint8(0, 1, 2, 3, 5, 7))
 	for i, myStruct := range structs {
 		gottenIndices := indices[myStruct.index]
 
 		debugMsg := fmt.Sprintf("gotten indices: %v vs myStruct at index %d %v", gottenIndices, i, myStruct)
-		require.True(t, len(gottenIndices) == len(myStruct.indicesExpected), fmt.Sprintf("lengths do not match: %v vs %v ", len(gottenIndices), len(myStruct.indicesExpected))+debugMsg)
+		require.True(t, len(gottenIndices) == len(myStruct.indicesExpected), fmt.Sprintf("lengths do not match: gotten indices len %v vs myStruct len %v ", len(gottenIndices), len(myStruct.indicesExpected))+debugMsg)
 		require.True(t, len(gottenIndices)%3 == 0)
 
-		for i := 0; i < len(myStruct.indicesExpected); i += 3 {
+		if i == 6 {
+			fmt.Print("here")
+		}
+		for iFromIndicesExpected := 0; iFromIndicesExpected < len(myStruct.indicesExpected); iFromIndicesExpected += 3 {
 
-			idx0 := gottenIndices[i]
-			idx1 := gottenIndices[i+1]
-			idx2 := gottenIndices[i+2]
+			idx0 := myStruct.indicesExpected[iFromIndicesExpected]
+			idx1 := myStruct.indicesExpected[iFromIndicesExpected+1]
+			idx2 := myStruct.indicesExpected[iFromIndicesExpected+2]
 			found := false
 			for j := 0; j < len(gottenIndices); j += 3 {
 				if gottenIndices[j] == idx0 && gottenIndices[j+1] == idx1 && gottenIndices[j+2] == idx2 {
